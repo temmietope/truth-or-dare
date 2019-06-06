1a6a7d30c6ed2f57961bd7986ea753d7f0cc2a16
Temitope <temmieayodele@gmail.com>
2019-06-06 20:24:10 +0100

    more

--- index.js
+++ index.js
@@ -1,30 +1,97 @@
-const selectTruth = document.querySelector(".select-truth");
+const startBtn = document.querySelector("#start-btn");
+const selectTruth = document.querySelector("#select-truth");
 const selectDare = document.querySelector("#select-dare");
-const truthChallenge = document.querySelector("#truth-challenge");
-const dareChallenge = document.querySelector("#dare-challenge");
+const challengeQ = document.querySelector("#challenge");
+const welcome = document.querySelector("#welcome");
+const selectAnOption = document.querySelector("#select-an-option");
+const questionSection1 = document.querySelector("#question-section1");
+const accept = document.querySelector("#accept");
+const decline = document.querySelector("#decline");
+const penalty = document.querySelector("#penalty");
+const mustDo = document.querySelector("#must-do");
+const back1 = document.querySelector("#back1");
+const back2 = document.querySelector("#back2");
+const backToHome = document.querySelector("#back-to-home");
+const timer = document.querySelector("#timer")
 
-let t = Math.round(Math.random() * challenge.truthquestions.length);
-let d = Math.round(Math.random() * challenge.dares.length)
-let pay = Math.round(Math.random() * challenge.penalty.length)
+console.log(startBtn);
+console.log(selectAnOption);
+console.log(questionSection1);
 
-function loadTruthChallenge(e){
-    truthChallenge.innerHTML = challenge.truthquestions[e];
+startBtn.addEventListener("click", () => {
+  welcome.style.display = "none";
+  selectAnOption.style.display = "block";
+  selectAnOption.classList.add = 'select-an-option';
+  console.log(selectAnOption.classList)
+});
+selectTruth.addEventListener("click", () => {
+  let t = Math.round(Math.random() * (challenge.truthquestions.length - 1));
+  selectAnOption.style.display = "none";
+  questionSection1.style.display = "block";
+  challengeQ.innerHTML = `<h3 class ='challenge-header'>TRUTH<h3>
+  ${challenge.truthquestions[t]}`;
+let time = 60;
+  interval= setInterval(()=>{
+    time = (time-1);
+    if(time===0){
+      clearInterval(interval);
+      showPenalty()
+    }
+    timer.innerHTML = `${time} seconds`
+    console.log(time)
+  }, 1000)
+
+
+});
+selectDare.addEventListener("click", () => {
+  let d = Math.round(Math.random() * (challenge.dares.length - 1));
+  selectAnOption.style.display = "none";
+  questionSection1.style.display = "block";
+  challengeQ.innerHTML = `<h3 class ='challenge-header'>DARE<h3>
+  ${challenge.dares[d]}`;
+  let time = 60;
+  interval = setInterval(()=>{
+    time = (time-1);
+    if(time==0){
+      clearInterval(interval)
+      showPenalty()
+    }
+    timer.innerHTML = `${time} seconds`
+    console.log(time)
+
+  }, 1000)
+});
+function showPenalty(){
+  questionSection1.style.display = "none";
+  penalty.style.display = "block";
+  clearInterval(interval);
+  let pay = Math.round(Math.random() * (challenge.penalty.length - 1));
+  mustDo.innerHTML = `${challenge.penalty[pay]}`;
 }
-function loadDareChallenge(e){
-    dareChallenge.innerHTML = challenge.dares[e];
-}
+decline.addEventListener("click", showPenalty);
+accept.addEventListener("click", () => {
+  questionSection1.style.display = "none";
+  selectAnOption.style.display = "block";
+  clearInterval(interval);
+  let t = Math.round(Math.random() * (challenge.truthquestions.length - 1));
+  let d = Math.round(Math.random() * (challenge.dares.length - 1));
+  let pay = Math.round(Math.random() * (challenge.penalty.length - 1));
+});
+back1.addEventListener("click", () => {
+  selectAnOption.style.display = "none";
+  welcome.style.display = "block";
+});
+back2.addEventListener("click", () => {
+  let t = Math.round(Math.random() * (challenge.truthquestions.length - 1));
+  let d = Math.round(Math.random() * (challenge.dares.length - 1));
+  clearInterval(interval);
+  timer.innerHTML = ``
+  questionSection1.style.display = "none";
+  selectAnOption.style.display = "block";
+});
+backToHome.addEventListener("click", () => {
+  penalty.style.display = "none";
+  selectAnOption.style.display = "block";
+});
 
-
-selectTruth.addEventListener('click', ()=>{
-    dareChallenge.style.display = 'none';
-    truthChallenge.style.display = 'block';
-    loadTruthChallenge(t);
-})
-selectDare.addEventListener('click', ()=>{
-    truthChallenge.style.display = 'none';
-    dareChallenge.style.display = 'block';
-    loadDareChallenge(d);
-})
-
-
-console.log(selectTruth)
+// window.history.back()
