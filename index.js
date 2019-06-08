f51eb3b5efcd46f6d5b5ac44d3c2049100faee21
Temitope <temmieayodele@gmail.com>
2019-06-08 02:19:49 +0100

    checked for errors

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const mustDo = document.querySelector("#must-do");
 const back1 = document.querySelector("#back1");
 const back2 = document.querySelector("#back2");
 const backToHome = document.querySelector("#back-to-home");
-const timer = document.querySelector("#timer")
+const timer = document.querySelector("#timer");
 
 console.log(startBtn);
 console.log(selectAnOption);
@@ -21,8 +21,8 @@ console.log(questionSection1);
 startBtn.addEventListener("click", () => {
   welcome.style.display = "none";
   selectAnOption.style.display = "block";
-  selectAnOption.classList.add = 'select-an-option';
-  console.log(selectAnOption.classList)
+  selectAnOption.classList.add = "select-an-option";
+  console.log(selectAnOption.classList);
 });
 selectTruth.addEventListener("click", () => {
   let t = Math.round(Math.random() * (challenge.truthquestions.length - 1));
@@ -30,18 +30,15 @@ selectTruth.addEventListener("click", () => {
   questionSection1.style.display = "block";
   challengeQ.innerHTML = `<h3 class ='challenge-header'>TRUTH<h3>
   ${challenge.truthquestions[t]}`;
-let time = 60;
-  interval= setInterval(()=>{
-    time = (time-1);
-    if(time===0){
+  let time = 60;
+  interval = setInterval(() => {
+    time = time - 1;
+    if (time === 0) {
       clearInterval(interval);
-      showPenalty()
+      showPenalty();
     }
-    timer.innerHTML = `${time} seconds  <i class="fas fa-exclamation"></i>`
-    console.log(time)
-  }, 1000)
-
-
+    timer.innerHTML = `${time} seconds  <i class="fas fa-exclamation"></i>`;
+  }, 1000);
 });
 selectDare.addEventListener("click", () => {
   let d = Math.round(Math.random() * (challenge.dares.length - 1));
@@ -50,18 +47,16 @@ selectDare.addEventListener("click", () => {
   challengeQ.innerHTML = `<h3 class ='challenge-header'>DARE<h3>
   ${challenge.dares[d]}`;
   let time = 60;
-  interval = setInterval(()=>{
-    time = (time-1);
-    if(time==0){
-      clearInterval(interval)
-      showPenalty()
+  interval = setInterval(() => {
+    time = time - 1;
+    if (time == 0) {
+      clearInterval(interval);
+      showPenalty();
     }
-    timer.innerHTML = `${time} seconds  <i class="fas fa-exclamation"></i>`
-    console.log(time)
-
-  }, 1000)
+    timer.innerHTML = `${time} seconds  <i class="fas fa-exclamation"></i>`;
+  }, 1000);
 });
-function showPenalty(){
+function showPenalty() {
   questionSection1.style.display = "none";
   penalty.style.display = "block";
   clearInterval(interval);
@@ -85,7 +80,7 @@ back2.addEventListener("click", () => {
   let t = Math.round(Math.random() * (challenge.truthquestions.length - 1));
   let d = Math.round(Math.random() * (challenge.dares.length - 1));
   clearInterval(interval);
-  timer.innerHTML = ``
+  timer.innerHTML = ``;
   questionSection1.style.display = "none";
   selectAnOption.style.display = "block";
 });
